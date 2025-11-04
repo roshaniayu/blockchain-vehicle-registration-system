7dc2f38c1208a89611b7e8e333c7f899911d4977
Hein-T.N <heinthetnaung@gmail.com>
2025-11-04 16:11:57 +0800

    Added: `/Marketplace` page.

---
+++ client/src/app/marketplace/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { VehiclesForSalePage } from "../vehicles/page";
+
+export default function MarketplacePage() {
+  return (
+    <div className="h-full">
+      <VehiclesForSalePage />
+    </div>
+  );
+}
